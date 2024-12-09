5f46988e89e3f72d9789cfcd8bb9cf1f3581d2dd
Sarah Rizvi <sarah.rizvi@stu.ocsb.ca>
2024-12-09 10:43:37 -0500

    Update Q3Addition.py

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,13 +1,14 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
-import random
-num = random.randint(0,100)
-print(num)
-num = random.randint(0,100)
-print(num)
-print('what is the sum of')
-input()
-if num + num: 
-    print('Correct!') 
+from random import randint
+
+a= randint(0, 100)
+b=randint(1, 100)
+
+for _ in range(10):
+    c = int(input(f"Enter your answer for {a} + {b}: "))
+    if c == a + b:
+        print("Correct!")
+        break
 else:
-    print('incorrect')
+    print("Correct Solutions!")
